9bdcf99cd5d195e834ccecc6b89aba213f7e8f97
Eduardo Abreu <edu.abreu09@gmail.com>
2025-08-21 21:06:41 -0300

    Initialize Spring Boot project with required configurations and dependencies.

---
+++ src/main/java/com/hexagonal/SolidPaymentsApiApplication.java
@@ -0,0 +1,11 @@
+package com.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidPaymentsApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SolidPaymentsApiApplication.class, args);
+    }
+}
