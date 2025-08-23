0b8f9fbc5f69bcb34e348b239c1ae8ce1163fa5e
Eduardo Abreu <edu.abreu09@gmail.com>
2025-08-23 16:11:28 -0300

    Rename `PagamentoController` to `PaymentController` and update method/variable names to English for better clarity and consistency.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 -- Este arquivo será executado pelo H2 em memória na inicialização
-CREATE TABLE PAGAMENTOS (
-                            ID VARCHAR(255) PRIMARY KEY,
-                            TIPO VARCHAR(50) NOT NULL,
-                            VALOR DECIMAL(19, 2) NOT NULL,
-                            STATUS VARCHAR(50) NOT NULL
+CREATE TABLE PAYMENTS (
+                          ID VARCHAR(255) PRIMARY KEY,
+                          PAYMENT_METHOD VARCHAR(50) NOT NULL,
+                          AMOUNT DECIMAL(19, 2) NOT NULL,
+                          STATUS VARCHAR(50) NOT NULL
 );
